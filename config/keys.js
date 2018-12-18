3944e0ed4cd84f8052b700529f3947f91ab3b7d9
Marco Antonio <marco@timais.com>
2018-12-18 10:52:51 -0200

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// keys.js - figure out what set of credentials to return
+if(process.env.NODE_ENV === 'production'){
+    //return the prod keys
+    module.exports = require('./prod');
+}
+else{
+    //return the dev keys
+    module.exports = require('./dev');
+}
